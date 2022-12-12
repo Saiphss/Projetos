82a4451ac7bf9b36c7f2d28dda1798e6c5563190
SAMUEL P R COSTA <samuelrdc12@gmail.com>
2022-12-12 10:12:39 -0300

    add script.py

---
+++ exercicios-python/ex1/script.py
@@ -0,0 +1,8 @@
+#Instrução auxiliar Break
+"""A Instrução auxiliar Break interrompe as repetições dos laços for e while, ou seja, ao encontrar uma instrução break a execução do looping irá ser interrompida.
+"""
+while True:
+    palavra = input("Digite 'Sair' para encerrar o laço: ")
+    if palavra == 'Sair':
+        break
+print("Comando aceito, o looping foi encerrado.")
