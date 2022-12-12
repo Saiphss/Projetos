5585b864bbe4f27b388524add84f2afc32aba9ba
SAMUEL P R COSTA <samuelrdc12@gmail.com>
2022-12-12 15:21:12 -0300

    Update
    Exercício.

--- exercicios-python/ex1/script_sub_pro.py
+++ exercicios-python/ex1/script_sub_pro.py
@@ -1,8 +1,8 @@
 escolha = input("Escolha, Digite '1' para a função 1 e '2' para a função 2: ")
 if escolha == "1":
-    def func1(x):
+    def func1(x): #cabeçalho da função func1 com o parâmetro formal.
         return x+1
-    s = func1(10)
+    s = func1(10) #aqui a função func1 é chamada com o parâmetro real 10.
 else:
     def func2(x):
         return x +2
