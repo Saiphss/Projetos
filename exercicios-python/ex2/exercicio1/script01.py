95843b4b0f2296b839015b291a231dcf07041892
SAMUEL P R COSTA <samuelrdc12@gmail.com>
2022-12-13 15:54:46 -0300

    Create script01.py

---
+++ exercicios-python/ex2/exercicio1/script01.py
@@ -0,0 +1,8 @@
+#Módulo math (dedicado a operações matemáticas)
+#Calculando a raiz quadrada de 5 por meio da função sqrt()
+import math
+print(math.sqrt(5))
+print(math.ceil(5))
+print(math.floor(5))
+print(math.cos(5))
+print(math.sin(5))
